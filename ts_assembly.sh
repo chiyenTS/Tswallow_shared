994e35e4eea39f980bd456771710256adb7975c1
chiyenTS <chiyents@gmail.com>
2019-02-24 01:34:14 -0600

    using supernova 2.1.1 and do 30mins test run 1GB ram

--- ts_assembly.sh
+++ ts_assembly.sh
@@ -3,10 +3,10 @@
 #SBATCH -J tsgenome_assembly
 #SBATCH -D /pylon5/mc5fs2p/chiyents/tsgenome/data/raw
 #SBATCH -o /pylon5/mc5fs2p/chiyents/tsgenome/scripts/assembly/ts_assembly-%j.o
-#SBATCH -e /pylon5/mc5fs2p/chiyents/tsgenome/scripts/assembly/ts_assembly-%j.o
+#SBATCH -e /pylon5/mc5fs2p/chiyents/tsgenome/scripts/assembly/ts_assembly-%j.e
 #SBATCH -N 1
 #SBATCH -p LM
-#SBATCH -t 2:00:00
+#SBATCH -t 0:30:00
 #SBATCH --mem=1000GB
 
 /pylon5/mc5fs2p/chiyents/program/supernova-2.1.1/supernova-cs/2.1.1/bin/run \
