4973230d3475e33000e8d06a8fe389943e322e22
chiyenTS <chiyents@gmail.com>
2019-02-24 02:15:39 -0600

    add export PATH to recongize the program

--- ts_assembly.sh
+++ ts_assembly.sh
@@ -3,14 +3,16 @@
 #SBATCH -J tsgenome_assembly
 #SBATCH -D /pylon5/mc5fs2p/chiyents/tsgenome/data/raw
 #SBATCH -o /pylon5/mc5fs2p/chiyents/tsgenome/scripts/assembly/ts_assembly-%j.o
-#SBATCH -e /pylon5/mc5fs2p/chiyents/tsgenome/scripts/assembly/ts_assembly-%j.e
+#SBATCH -e /pylon5/mc5fs2p/chiyents/tsgenome/scripts/assembly/ts_assembly-%j.o
 #SBATCH -N 1
 #SBATCH -p LM
 #SBATCH -t 0:30:00
 #SBATCH --mem=1000GB
 
-/pylon5/mc5fs2p/chiyents/program/supernova-2.1.1/supernova-cs/2.1.1/bin/run \
---id tsgenome_testrun \
+export PATH=/pylon5/mc5fs2p/chiyents/program/supernova-2.1.1:$PATH
+
+supernova run \
+--id tsgenome1 \
 --maxreads 485000000 \
 --fastqs /pylon5/mc5fs2p/chiyents/tsgenome/data/raw \
 --localcores=28 
