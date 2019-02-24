7e699c6af766fdac3c2ccbb3710a6368cb0f9c6c
chiyenTS <chiyents@gmail.com>
2019-02-23 19:24:52 -0600

    using supernova 2.1.1 and submit for a testrun, 2TB, 2hr

--- ts_assembly.sh
+++ ts_assembly.sh
@@ -6,11 +6,11 @@
 #SBATCH -e /pylon5/mc5fs2p/chiyents/tsgenome/scripts/assembly/ts_assembly-%j.o
 #SBATCH -N 1
 #SBATCH -p LM
-#SBATCH -t 4:00:00
+#SBATCH -t 2:00:00
 #SBATCH --mem=1000GB
 
-/pylon5/mc5fs2p/chiyents/program/supernova-2.0.0/supernova-cs/2.0.0/bin/run \
---id tsgenome1 \
+/pylon5/mc5fs2p/chiyents/program/supernova-2.1.1/supernova-cs/2.1.1/bin/run \
+--id tsgenome_testrun \
 --maxreads 485000000 \
 --fastqs /pylon5/mc5fs2p/chiyents/tsgenome/data/raw \
 --localcores=28 
